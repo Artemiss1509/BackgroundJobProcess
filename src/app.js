f0dbf5066fafdad9430ec220ab090dfbd5cea3ca
Artemiss1509 <stanza-grout54@icloud.com>
2026-04-30 13:14:54 +0530

    Database initialized

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const { initializeDatabase } = require('./config/database');
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,7 @@ app.use(cors());
 
 async function startServer() {
   try {
+    await initializeDatabase()
     app.listen(port, () => {
       logger.info('AsyncFlow API started', { port });
     });
